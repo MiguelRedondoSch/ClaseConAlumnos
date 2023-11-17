ccfdaec832562a2c43bed9e05ef8025b6a8ed921
Miguel Redondo <11308sch@gmail.com>
2023-11-17 14:22:01 +0100

    Creacion de clase y contructor de Clase

---
+++ Clase.java
@@ -0,0 +1,11 @@
+public class Clase {
+    private String tutor;
+    private  String asignatura;
+    private Alumno alumno;
+
+    public Clase(String tutor, String asignatura, Alumno alumno) {
+        this.tutor = tutor;
+        this.asignatura = asignatura;
+        this.alumno = alumno;
+    }
+}

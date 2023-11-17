53f412470b7730497c913bfacf87a3210aa2cbc2
Miguel Redondo <11308sch@gmail.com>
2023-11-17 14:28:09 +0100

    Creacion de clase y contructor de Asignatura

---
+++ Asignatura.java
@@ -0,0 +1,9 @@
+public class Asignatura {
+    private String nombre;
+    private  int horasSemanales;
+
+    public Asignatura(String nombre, int horasSemanales) {
+        this.nombre = nombre;
+        this.horasSemanales = horasSemanales;
+    }
+}

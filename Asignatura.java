ec587b2e958944c2bbeedb568787b669347b2586
KevinSilvares <k.silvares24@gmail.com>
2023-11-20 09:36:09 +0100

    Completada Asignatura.java

--- Asignatura.java
+++ Asignatura.java
@@ -6,4 +6,20 @@ public class Asignatura {
         this.nombre = nombre;
         this.horasSemanales = horasSemanales;
     }
+    
+     public String getNombre(){
+        return nombre;
+    }
+    
+    public int getHorasSemanales(){
+        return horasSemanales;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setHorasSemanales(int horasSemanales){
+        this.horasSemanales = horasSemanales;
+    }
 }

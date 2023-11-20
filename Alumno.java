3d7399322d16dd7a229b055be1eafee19dc89367
Miguel Redondo <11308sch@gmail.com>
2023-11-20 09:49:46 +0100

    getter y setter de alumno

--- Alumno.java
+++ Alumno.java
@@ -1,11 +1,35 @@
 public class Alumno {
     private String nombre;
-    private  int edad;
+    private int edad;
     private Asignatura asignatura;
 
     public Alumno(String nombre, int edad, Asignatura asignatura) {
         this.nombre = nombre;
         this.edad = edad;
+        this.asignatura = asignatura;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public Asignatura getAsignatura(){
+        return asignatura;
+    }
+
+    public void setNombre(string nombre){
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setAsignatura(Asignatura asignatura){
         this.asignatura = asignatura;
     }
 }
